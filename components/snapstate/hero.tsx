9848079f27da346d75a7ee2927e765a15a3d9c5c
Arush Wadhawan <warush23+github@gmail.com>
2026-05-29 18:15:45 -0400

    feat: update download links to latest version v2.3.1

--- components/snapstate/hero.tsx
+++ components/snapstate/hero.tsx
@@ -96,7 +96,7 @@ export function Hero() {
           }}
         >
           <a
-            href="https://github.com/SoulSniper-V2/SnapState/releases/download/v2.3.0/SnapState.dmg"
+            href="https://github.com/SoulSniper-V2/SnapState/releases/download/v2.3.1/SnapState.dmg"
             style={{
               display: "inline-flex",
               alignItems: "center",
